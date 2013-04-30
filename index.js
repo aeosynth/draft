8bb68a00d168b32b2bd47bceef7341aa739688b8
James Campos <james.r.campos@gmail.com>
2013-04-30 06:26:47 -0700

    shuffle, .gitignore lib

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var http = require('http');
 var express = require('express');
 var engine = require('engine.io');
 var router = require('./lib/router');
-var genDeck = require('./lib/genDeck');
+var genDeck = require('./lib/generate/deck');
 
 // TODO replace express w/ static file server
 var app = express()
