d6cbd490c5f12ffe394ed4fa8a674e72e1f827fb
James Campos <james.r.campos@gmail.com>
2014-08-26 07:24:23 -0700

    ionicons

--- public/src/lobby.js
+++ public/src/lobby.js
@@ -26,12 +26,18 @@ var Lobby = React.createClass({
 
   render() {
     return d.div({},
-      d.a({href:'https://github.com/aeosynth/draft'}, d.img({id: 'github', src:'https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67', alt:'Fork me on GitHub', 'data-canonical-src':'https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png'})),
       Chat(this.state),
       d.h1({}, 'drafts.in'),
       d.p({className: 'err'}, App.state.err),
-      d.div({}, d.small({}, 'unaffiliated with wizards of the coast')),
-      Create())
+      Create(),
+      d.footer({},
+        d.div({},
+          d.a({ className: 'icon ion-social-github', href: 'https://github.com/aeosynth/draft' }),
+          d.a({ className: 'icon ion-social-twitter', href: 'https://twitter.com/aeosynth' }),
+          d.a({ className: 'icon ion-android-mail', href: 'mailto:james.r.campos@gmail.com' })),
+        d.div({},
+          d.small({}, 'unaffiliated with wizards of the coast')))
+    )
   }
 })
 
