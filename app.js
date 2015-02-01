a49c500d90344f5044ef57381db7946391c9d723
James Campos <james.r.campos@gmail.com>
2015-02-01 11:14:59 -0800

    rm logs

--- app.js
+++ app.js
@@ -14,21 +14,4 @@ var server = http.createServer(function(req, res) {
 }).listen(PORT)
 var eioServer = eio(server).on('connection', router)
 
-var HOUR_S = 60 * 60 // hour in seconds
-
-console.log('listening on port', PORT)
-
-;(function log() {
-  var now = (new Date).toUTCString()
-  var up = Math.floor(process.uptime() / HOUR_S)
-  var mem = process.memoryUsage()
-  var count = eioServer.clientsCount
-
-  for (var key in mem)
-    mem[key] = Math.round(mem[key] / 1e6)
-
-  console.log('[%s]\t%d\t%d\t%d\t%d\t%d',
-    now, up, count, mem.rss, mem.heapTotal, mem.heapUsed)
-
-  setTimeout(log, 1000 * HOUR_S)
-})()
+console.log(new Date)
