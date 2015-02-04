b635a23613c1dc64608da9af8b00f91d3a53425a
Stephen Rugh <stephenrugh@gmail.com>
2015-02-04 01:37:07 -0600

    Code cleanup

--- public/src/cards.js
+++ public/src/cards.js
@@ -95,9 +95,8 @@ let events = {
       pack[name] = 1
     }
     App.update()
-    if (App.state.beep){
-      self.beep.play()
-    }
+    if (App.state.beep)
+      document.getElementById('beep').play()
   },
   create() {
     let {type, seats} = App.state
