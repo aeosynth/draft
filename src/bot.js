fe44d577f36a3605749cf4ffa747f5b9ebdd9558
tritoch <tritochs@gmail.com>
2016-04-29 11:38:10 -0500

    Bots randomly pick for unscored sets
    Previous version had bots picking the first card or the only scored card for reprints.  Still need to address cube.

--- src/bot.js
+++ src/bot.js
@@ -12,12 +12,29 @@ module.exports = class extends EventEmitter {
   getPack(pack) {
     var score = 99
     var index = 0
+    var cardcount = 0
+    var scoredcards = 0
     pack.forEach((card, i) => {
-      if (card.score < score) {
-        score = card.score
-        index = i
-      }})
-    pack.splice(index, 1)
+      if (card.score) {
+        //if there's a better card in the pack, save the index of that card
+        if (card.score < score) {
+          score = card.score
+          index = i
+        }
+      //keep track of the # of cards with scores
+      scoredcards = scoredcards + 1
+      }
+      //track the number of cards in the pack
+      cardcount = i
+    })
+    //if 50% of cards doesn't have a score, we're going to pick randomly
+    if (scoredcards / cardcount < .5) {
+      var randpick = _.rand(cardcount)
+      pack.splice(randpick, 1)
+    }
+    else {
+      pack.splice(index, 1)
+    }
     this.emit('pass', pack)
   }
   send(){}
